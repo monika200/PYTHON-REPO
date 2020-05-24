6d46f37163d78e6721f31ebfeea4ed96aad43705
MONIKA SHARMA <63012770+monika200@users.noreply.github.com>
2020-05-25 01:04:05 +0530

    PYTHON DAY-2

--- Day2.py
+++ Day2.py
@@ -1,5 +1,8 @@
 
-Que:1.
+Que:1.Python Program to Read a Number n And Print the Series “1+2+…..+n= “
+sample:Enter a number: 4
+1 + 2 + 3 + 4 = 10 
+
 n=int(input("Enter a no:"))
 sum=0
 for i in range(1,n+1):
@@ -9,13 +12,20 @@ for i in range(1,n+1):
         print("+",end=" ")
 print("=",sum)
 
+-------------------------------------------------------------------------------------------------
+
+Que:2.Write a Python program to count the number of even and odd numbers from a series of numbers.
+Sample:
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+Expected Output :
+Number of even numbers : 4
+Number of odd numbers : 5
 
 
-Que:2.
- numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
- even_count=0
- odd_count=0
- for i in numbers:
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+even_count=0
+odd_count=0
+for i in numbers:
     if i%2==0:
         even_count=even_count+1
     else:
@@ -23,20 +33,39 @@ Que:2.
 print("No of even no is:",even_count)
 print("No of odd no is:",odd_count)
 
+--------------------------------------------------------------------------------------------------
+
+Que:3.Write a Python program to create the multiplication table (from 1 to 10) of a number. 
 
 
-Que:3.
 n=int(input("Enter a no:"))
 for i in range(1,11):
     print(n*i)
-    
-Que.4.
+ 
 
-def SumOddNumbers(n):
+--------------------------------------------------------------------------------------------------
+
+Que.4.Given the triangle of consecutive odd  numbers 
+Example we call function :- row_sum_odd_numbers(2)
+Result will be=3 + 5 = 8
+if user give 4 then ur output is 13 + 15 + 17+ 19 = 64
+
+
+def rowSumOddNumbers(n):
   return n*n*n
 print(SumOddNumbers(4))
+ 
+-------------------------------------------------------------------------------------------------   
     
-Que:5.
+    
+Que:5. Write python program to print the pattern given below
+Note: Take input from user
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
 n=int(input("Enter a no:"))
 for i in range(1,n+1):
     for j in range(i):
