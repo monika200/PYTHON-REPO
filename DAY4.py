fb04a3188729c8092fe76fdd47fa6d09739a6160
MONIKA SHARMA <63012770+monika200@users.noreply.github.com>
2020-05-26 23:30:59 +0530

    PYTHON DAY-4

--- DAY4.py
+++ DAY4.py
@@ -29,7 +29,7 @@ Que.3.	Write a recursive function to calculate the sum of numbers from 0 to 10
 Expected output: 55
 
 def sum(n):
-    if n==0:
+    if n<=0:
         return n
     else:
         return n + sum(n-1)
